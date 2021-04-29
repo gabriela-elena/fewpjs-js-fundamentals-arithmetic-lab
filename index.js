49a26bca2112de5ac79238bdb1c3bd8a9761d72c
Gabriela <gabrielaelenadesigns@gmail.com>
2021-04-28 22:33:38 -0700

    commit tree 6619c6842e83ed859698ee94dd9862456d32bf67

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 109;
+let num4 = 21;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = 20
